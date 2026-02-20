7d27c58da134a89d160c712ef25228f6eef8dc58
ryanseery <ryanpseery@gmail.com>
2026-02-19 22:05:03 -0500

    feat(main): theme

--- src/app/[topic]/[year]/index.tsx
+++ src/app/[topic]/[year]/index.tsx
@@ -1,10 +1,21 @@
 import { Redirect, Stack } from "expo-router";
+import { useEffect } from "react";
 
 import { useParams } from "hooks/use-params";
 import { Topic } from "screens/topic";
+import { useTheme } from "utils/theme";
 
 export default function TopicIndex() {
   const { topic, year, sessionId } = useParams();
+  const { themeName, setTheme } = useTheme();
+
+  // set theme for invited user
+  // get strict
+  useEffect(() => {
+    if (topic && topic.value !== themeName) {
+      setTheme(topic.value);
+    }
+  }, []);
 
   if (!topic || !year) {
     return <Redirect href="/" />;
