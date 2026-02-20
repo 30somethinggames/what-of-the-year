6d55340272ddc25b345954f19774981400ee7911
ryanseery <ryanpseery@gmail.com>
2026-02-20 17:43:50 -0500

    feat(main): remove theme

--- src/app/[topic]/[year]/index.tsx
+++ src/app/[topic]/[year]/index.tsx
@@ -1,25 +1,12 @@
 import { Redirect, Stack } from "expo-router";
-import { useEffect } from "react";
 
 import { useParams } from "hooks/use-params";
 import { Topic } from "screens/topic";
-import { useTheme } from "utils/theme";
 
 export default function TopicIndex() {
   const { topic, year, sessionId } = useParams();
-  const { themeName, setTheme } = useTheme();
 
-  // set theme for invited user
-  // get strict
-  useEffect(() => {
-    if (topic && topic.value !== themeName) {
-      setTheme(topic.value);
-    }
-  }, []);
-
-  if (!topic || !year) {
-    return <Redirect href="/" />;
-  }
+  if (!topic || !year) return <Redirect href="/" />;
 
   return (
     <>
