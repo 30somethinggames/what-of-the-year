11d2238d5745db8941cc7cbefc9e53777a8d4f20
ryanseery <ryanpseery@gmail.com>
2026-03-04 00:39:41 -0500

    feat(main): animation / assets

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,6 +1,7 @@
 import { Link } from "expo-router";
 import { useState } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import Animated from "react-native-reanimated";
 
 import { Button } from "components/button";
 import { Container } from "components/container";
@@ -9,48 +10,55 @@ import { type TopicType, topics } from "constants/topics";
 import { type Year, years } from "constants/years";
 import { createStyles, themes } from "utils/theme";
 
+import { useHomeAnimation } from "./use-home-animation";
+
 export function Home() {
   const [topic, setTopic] = useState<TopicType>(topics[0]);
   const [year, setYear] = useState<Year>(years[0]);
   const s = useStyles({ backgroundColor: themes[topic.value].topic.color });
-
-  const onTopicChange = (v: TopicType) => setTopic(v);
-  const onYearChange = (v: Year) => setYear(v);
+  const { topicStyle, yearStyle, ofStyle, btnStyle } = useHomeAnimation();
 
   return (
-    <Container>
-      <View style={s.root}>
-        <Picker testID="topic-picker" data={topics} value={topic} onValueChange={onTopicChange} />
-        <Text style={s.of}>of</Text>
-        <Picker testID="year-picker" data={years} value={year} onValueChange={onYearChange} />
-      </View>
+    <Animated.View style={s.bg}>
+      <Container>
+        <View style={s.root}>
+          <Animated.View style={[s.pickerWrapper, topicStyle]}>
+            <Picker testID="topic-picker" data={topics} value={topic} onValueChange={setTopic} />
+          </Animated.View>
 
-      <Link
-        asChild
-        href={{
-          pathname: "/[topic]/[year]",
-          params: { topic: topic.value, year: year.value },
-        }}
-      >
-        <Button label="Start" style={s.btn} />
-      </Link>
-    </Container>
+          <Animated.Text style={[s.of, ofStyle]}>of</Animated.Text>
+
+          <Animated.View style={[s.pickerWrapper, yearStyle]}>
+            <Picker testID="year-picker" data={years} value={year} onValueChange={setYear} />
+          </Animated.View>
+        </View>
+
+        <Animated.View style={btnStyle}>
+          <Link
+            asChild
+            href={{ pathname: "/[topic]/[year]", params: { topic: topic.value, year: year.value } }}
+          >
+            <Button label="Start" style={s.btn} />
+          </Link>
+        </Animated.View>
+      </Container>
+    </Animated.View>
   );
 }
 
 const useStyles = createStyles((t, p: { backgroundColor: string }) => ({
+  bg: { flex: 1 },
   root: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
+  pickerWrapper: { width: "100%" },
   of: {
     fontFamily: t.text.font.semibold,
     fontSize: 52,
     color: t.colors.black100,
     marginVertical: t.spacing.lg,
   },
-  btn: {
-    backgroundColor: p.backgroundColor,
-  },
+  btn: { backgroundColor: p.backgroundColor },
 }));
